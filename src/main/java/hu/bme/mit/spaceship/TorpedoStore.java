7c283a16d5edd84bc7512f8e0c5456765af14e12
kaszamarci0417 <kaszamarci0417@gmail.com>
2025-10-08 14:53:13 +0200

    Fixed Random most mar tenyleg jo esku

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -39,6 +39,7 @@ public class TorpedoStore {
 
     // simulate random overheating of the launcher bay which prevents firing
     double r = random.nextDouble();
+    // Ez igy jo, legyszi ne legyel butus
 
     if (r >= FAILURE_RATE) {
       // successful firing
